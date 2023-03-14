7ec470950615657f9b9acda234479802e5f8ac0c
Jens Meisner <jnsmsnr@gmail.com>
2023-03-14 13:44:10 +0100

    exporting SystemSetupStatusComponent

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import { TextareaComponent, TextareaComponentProps } from "./general/index.js";
 import {
   SystemSetupResultComponent,
   SystemSetupResultComponentProps,
+  SystemSetupStatusComponent,
+  SystemSetupStatusComponentProps,
 } from "./setup/index.js";
 
-export { SystemSetupResultComponent, SystemSetupResultComponentProps };
+export {
+  SystemSetupResultComponent,
+  SystemSetupResultComponentProps,
+  SystemSetupStatusComponent,
+  SystemSetupStatusComponentProps,
+};
 export { TextareaComponent, TextareaComponentProps };
